ad3664f03f8db78ad6f1d232864d3132a7eebd76
dclarkasu <dclark205@gmail.com>
2017-09-30 12:42:04 -0600

    add Activity entity

---
+++ FlakJPA/src/entities/Activity.java
@@ -0,0 +1,127 @@
+package entities;
+
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Activity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name; 
+	private String description;
+	@Column(name="timestamp")
+	private Date timeStamp;
+	@Column(name="assigned")
+	private boolean isAssigned;
+	@Column(name="complete")
+	private boolean isComplete;
+	@ManyToMany(cascade= {CascadeType.PERSIST, CascadeType.REMOVE})
+	@JoinTable(name="user_activity",
+	joinColumns=@JoinColumn(name="activity_id"),
+	inverseJoinColumns=@JoinColumn(name="user_id")
+	)
+	List<User> users;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(Date timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public boolean isAssigned() {
+		return isAssigned;
+	}
+	public void setAssigned(boolean isAssigned) {
+		this.isAssigned = isAssigned;
+	}
+	public boolean isComplete() {
+		return isComplete;
+	}
+	public void setComplete(boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+	public List<User> getUsers() {
+		return users;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	public int getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", name=" + name + ", description=" + description + ", timeStamp=" + timeStamp
+				+ ", isAssigned=" + isAssigned + ", isComplete=" + isComplete + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + (isAssigned ? 1231 : 1237);
+		result = prime * result + (isComplete ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((timeStamp == null) ? 0 : timeStamp.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Activity other = (Activity) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (isAssigned != other.isAssigned)
+			return false;
+		if (isComplete != other.isComplete)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (timeStamp == null) {
+			if (other.timeStamp != null)
+				return false;
+		} else if (!timeStamp.equals(other.timeStamp))
+			return false;
+		return true;
+	}
+	
+	
+	}
+	
--- FlakJPA/src/entities/entity.java
+++
@@ -1,5 +0,0 @@
-package entities;
-
-public class entity { ///CHANGE THIS NAME!!!!!!
-
-}
