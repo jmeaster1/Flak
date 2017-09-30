0333169d66cdc061554c5f90c86f08e3a83d8ffd
Meira Pentermann <meira@meirapentermann.com>
2017-09-30 14:01:08 -0600

    started testing file

---
+++ FlakJPA/test/tests/EntitiesTesting.java
@@ -0,0 +1,39 @@
+package tests;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import entities.Group;
+
+public class EntitiesTesting {
+	private EntityManagerFactory emf = null;
+	private EntityManager em = null;
+	
+	@Before
+	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("VideoStore");
+		em = emf.createEntityManager();
+	}
+	
+	@After
+	public void tearDown() {
+		em.close();
+		emf.close();
+	}
+	
+	@Test
+	public void smoke_test() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	public void test_group_name_mapping() {
+		Group g = em.find(Group.class, 1);
+		assertEquals(g.getName(), "FlakFamily");
+	}
+
+}
