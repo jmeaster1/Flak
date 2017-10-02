6f13432f2ba075debeec483ad26430c8ae2396a5
Meira Pentermann <meira@meirapentermann.com>
2017-10-02 09:17:45 -0600

    stub out DAO methods

--- Flak/src/data/FlakDAO.java
+++ Flak/src/data/FlakDAO.java
@@ -1,10 +1,96 @@
 package data;
 
+import java.util.List;
+
+import entities.Activity;
+import entities.Contact;
+import entities.Conversation;
+import entities.Group;
+import entities.PhoneNumber;
+import entities.Post;
+import entities.QRL;
+import entities.Type;
 import entities.User;
 
 public interface FlakDAO {
 	
+	// show by id for all 9 entities;
+	
+	public Group showGroup(int id);
 	
 	public User showUser(int id);
+	
+	public Post showPost(int id);
+	
+	public Conversation showConversation(int id);
+	
+	public Activity showActivity(int id);
+	
+	public Type showType(int id);
+	
+	public Contact showContact(int id);
+	
+	public PhoneNumber showPhoneNumber(int id);
+	
+	public QRL showQRL(int id);
+	
+	
+	public List<Activity> getActivitiesByType(String type);
+	
+	//Create, Edit, Delete for all 9 items
+	
+	public Group createGroup(Group group);
+	
+	public Group editGroup(Group group);
+	
+	public boolean deleteGroup(int id); //only available to Flak Administration
+	
+	public User createUser(User user);
+	
+	public User editUser(User user);
+	
+	public boolean deleteUser(int id);
+	
+	public Post createPost(Post post);
+	
+	public Post editPost(Post post);
+	
+	public boolean deletePost(int id);
+
+	public Conversation createConversation(Conversation conv);
+	
+	public Conversation editConversation(Conversation conv);
+	
+	public boolean deleteConversation(int id);
+	
+	public Activity createActivity(Activity activity);
+	
+	public Activity editActivity(Activity activity);
+	
+	public boolean deleteActivity(int id);
+	
+	public Type createType(Type type);
+	
+	public Type editType(Type type);
+	
+	public boolean deleteType(int id);
+	
+	public Contact createContact(Contact contact);
+	
+	public Contact editContact(Contact contact);
+	
+	public boolean deleteContact(int id);
+	
+	public PhoneNumber createPhoneNumber(PhoneNumber phone);
+	
+	public PhoneNumber editPhoneNumber(PhoneNumber phone);
+	
+	public boolean deletePhoneNumber(int id);
+	
+	public QRL createQRL(QRL qrl);
+	
+	public QRL editQRL(QRL qrl);
+	
+	public boolean deleteQRL(int id);
 	
 }
--- Flak/src/data/FlakDAOImpl.java
+++ Flak/src/data/FlakDAOImpl.java
@@ -1,10 +1,20 @@
 package data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
 
+import entities.Activity;
+import entities.Contact;
+import entities.Conversation;
+import entities.Group;
+import entities.PhoneNumber;
+import entities.Post;
+import entities.QRL;
+import entities.Type;
 import entities.User;
 
 @Component
@@ -16,6 +26,222 @@ public class FlakDAOImpl implements FlakDAO {
 	public User showUser(int id) {
 		return em.find(User.class, id);
 		
+	}
+
+	@Override
+	public Group showGroup(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Post showPost(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Conversation showConversation(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity showActivity(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type showType(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact showContact(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PhoneNumber showPhoneNumber(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QRL showQRL(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Activity> getActivitiesByType(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Group createGroup(Group group) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Group editGroup(Group group) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteGroup(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User createUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User editUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Post createPost(Post post) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Post editPost(Post post) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deletePost(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Conversation createConversation(Conversation conv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Conversation editConversation(Conversation conv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteConversation(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Activity createActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Activity editActivity(Activity activity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteActivity(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Type createType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type editType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteType(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Contact createContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contact editContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteContact(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PhoneNumber createPhoneNumber(PhoneNumber phone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PhoneNumber editPhoneNumber(PhoneNumber phone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deletePhoneNumber(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public QRL createQRL(QRL qrl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public QRL editQRL(QRL qrl) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteQRL(int id) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
