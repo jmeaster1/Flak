d046143021622488e7a0bf06f619b0c623612e8c
dclarkasu <dclark205@gmail.com>
2017-10-02 12:38:13 -0600

    controller jsp routing methods

--- Flak/src/controllers/FlakController.java
+++ Flak/src/controllers/FlakController.java
@@ -17,7 +17,15 @@ public class FlakController {
 
 	@RequestMapping(path = "index.do", method = RequestMethod.GET) // unfinished view
 	public String show( Model model) {
-		return "index.jsp";
+//		return "index.jsp";
+//		return "calendar.jsp";
+//		return "dashboard.jsp";
+//		return "messagebrd.jsp";
+//		return "regstatus.jsp";
+//		return "qrl.jsp";
+//		return "grouplist.jsp";
+//		return "shopping.jsp";
+		return "task.jsp";
 	}
 	@RequestMapping(path = "login.do", method = RequestMethod.GET) // unfinished view
 	public String login( Model model) {
@@ -28,7 +36,7 @@ public class FlakController {
 		return "signup.jsp";
 	}
 
-	@RequestMapping(path = "", method = RequestMethod.GET)
+	@RequestMapping(path = "dashboard.do", method = RequestMethod.GET)
 	public String dashboard(Model model) {
 		return "dashboard.jsp";
 	}
@@ -36,13 +44,42 @@ public class FlakController {
 	@RequestMapping(path = "about.do", method = RequestMethod.GET) // unfinished view
 	public String about( Model model) {
 		return "about.jsp";
-
+	}	
+	
+	@RequestMapping(path = "calendar.do", method = RequestMethod.GET) // unfinished view
+	public String calendar( Model model) {
+		return "calendar.jsp";
+	}	
+	
+	@RequestMapping(path = "grouplist.do", method = RequestMethod.GET) // unfinished view
+	public String groupList( Model model) {
+		return "grouplist.jsp";
+	}	
+	
+	@RequestMapping(path = "messagebrd.do", method = RequestMethod.GET) // unfinished view
+	public String messageBoard( Model model) {
+		return "messagebrd.jsp";
+	}	
+	
+	@RequestMapping(path = "qrl.do", method = RequestMethod.GET) // unfinished view
+	public String QRL( Model model) {
+		return "qrl.jsp";
+	}	
+	
+	@RequestMapping(path = "regstatus.do", method = RequestMethod.GET) // unfinished view
+	public String regStatus( Model model) {
+		return "regstatus.jsp";
 	}
 	
+	@RequestMapping(path = "shopping.do", method = RequestMethod.GET) // unfinished view
+	public String shopping( Model model) {
+		return "shopping.jsp";
+	}
 	
-	
-	
-	
+	@RequestMapping(path = "task.do", method = RequestMethod.GET) // unfinished view
+	public String task( Model model) {
+		return "task.jsp";
+	}	
 	
 	@RequestMapping(path="GetLoginForm.do", method=RequestMethod.GET)// form validation
 	public ModelAndView getLoginForm() {
